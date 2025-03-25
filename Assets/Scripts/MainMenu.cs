420ce119f57e5cf4dbe33a28530b16eb74f3bfaf
FinleyLeo <550017@chrome.escg.ac.uk>
2025-03-25 22:50:50 +0000

    Quit Confirmation and settings fixes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public TextMeshProUGUI startText;
 
     Animator anim;
-    public Animator mountainAnim;
-
+    public Animator mountainAnim, settings, quitConfirm;
     float startdelay;
+
+    [Header("Slider stuff")]
+    public GameObject musicCross;
+    public GameObject sfxCross;
+    public Image sfxBack, musicBack, sfxFill, musicFill;
+    bool sfxMuted, musicMuted;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,7 +29,7 @@ public class MainMenu : MonoBehaviour
     {
         startdelay += Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.X) && startdelay >= 2f)
+        if (Input.GetKeyDown(KeyCode.X) && startdelay >= 1.5f)
         {
             StartCoroutine(FlashText());
         }
@@ -57,7 +64,7 @@ public class MainMenu : MonoBehaviour
         mountainAnim.SetInteger("Break", 1);
         mountainAnim.SetBool("InMenu", true);
 
-        SceneSwitcher.instance.Transition("Loop1");
+        _SceneSwitcher.instance.Transition("Loop1");
     }
     public void OpenOptions()
     {
@@ -65,6 +72,8 @@ public class MainMenu : MonoBehaviour
         print("Opening options...");
         mountainAnim.SetInteger("Break", 2);
         mountainAnim.SetBool("InMenu", true);
+
+        settings.SetBool("Opened", true);
     }
 
     public void QuitGame()
@@ -73,10 +82,73 @@ public class MainMenu : MonoBehaviour
         print("Quitting game...");
         mountainAnim.SetInteger("Break", 3);
         mountainAnim.SetBool("InMenu", true);
+
+        quitConfirm.SetBool("Opened", true);
+    }
+
+    public void ConfirmQuit()
+    {
+        mountainAnim.SetBool("InMenu", false);
+        quitConfirm.SetBool("Opened", false);
+
+        Application.Quit();
+    }
+
+    public void CancelQuit()
+    {
+        mountainAnim.SetBool("InMenu", false);
+        quitConfirm.SetBool("Opened", false);
+    }
+
+    public void MusicToggle()
+    {
+        musicMuted = !musicMuted;
+
+        musicCross.SetActive(musicMuted);
+
+        if (musicMuted)
+        {
+            musicBack.color = new Color(0.75f, 0.75f, 0.75f);
+            musicFill.color = new Color(0.75f, 0.75f, 0.75f);
+
+            musicFill.GetComponentInParent<Slider>().interactable = false;
+        }
+
+        else
+        {
+            musicBack.color = Color.white;
+            musicFill.color = Color.white;
+
+            musicFill.GetComponentInParent<Slider>().interactable = true;
+        }
+    }
+
+    public void SFXToggle()
+    {
+        sfxMuted = !sfxMuted;
+
+        sfxCross.SetActive(sfxMuted);
+
+        if (sfxMuted)
+        {
+            sfxBack.color = new Color(0.75f, 0.75f, 0.75f);
+            sfxFill.color = new Color(0.75f, 0.75f, 0.75f);
+
+            sfxFill.GetComponentInParent<Slider>().interactable = false;
+        }
+
+        else
+        {
+            sfxBack.color = Color.white;
+            sfxFill.color = Color.white;
+
+            sfxFill.GetComponentInParent<Slider>().interactable = true;
+        }
     }
 
     public void ExitSettings()
     {
-
+        mountainAnim.SetBool("InMenu", false);
+        settings.SetBool("Opened", false);
     }
 }
